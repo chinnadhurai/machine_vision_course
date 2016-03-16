42af957d3aad5997e0a1e914e83acd7441d935a8
Chinna <chinnadhurai@gmail.com>
2016-03-15 20:35:23 -0400

    Update run_exp.sh

--- hw2/run_exp.sh
+++ hw2/run_exp.sh
@@ -7,6 +7,6 @@ do
   THEANO_FLAGS='floatX=float32,device=gpu6' python run_exp.py q1 2000 $alpha
   THEANO_FLAGS='floatX=float32,device=gpu6' python run_exp.py q2 2000 $alpha
   THEANO_FLAGS='floatX=float32,device=gpu6' python run_exp.py q1 5000 $alpha
-  THEANO_FLAGS='floatX=float32,device=gpu6' python run_exp.py q1 5000 $alpha
+  THEANO_FLAGS='floatX=float32,device=gpu6' python run_exp.py q2 5000 $alpha
 done
 rm -rf /data/lisatmp4/chinna/data/ift6268/logging/plotq*
