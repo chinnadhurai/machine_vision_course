9f0551cf0fa58d10390a315dc4614a9bcc149f5b
Chinna <chinnadhurai@gmail.com>
2016-03-19 17:25:48 -0400

    Update run_exp.sh

--- hw2/run_exp.sh
+++ hw2/run_exp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source init_setup.sh
 for alpha in 0.3 0.5 0.7 0.9
-for n in 10000 20000 15000
 do
-do
-  THEANO_FLAGS='floatX=float32,device=gpu6' python run_exp.py q1a $n $alpha 
-  THEANO_FLAGS='floatX=float32,device=gpu6' python run_exp.py q1b $n $alpha
-done
+  for n in 10000 20000 15000
+  do
+    THEANO_FLAGS='floatX=float32,device=gpu6' python run_exp.py q1a $n $alpha 
+    THEANO_FLAGS='floatX=float32,device=gpu6' python run_exp.py q1b $n $alpha
+  done
 done
 rm -rf /data/lisatmp4/chinna/data/ift6268/logging/plotq*
